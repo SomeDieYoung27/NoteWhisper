980f3faf4cf0dbd29ea09de0a205ba90bbcf17f1
Shashwat <shashwat12028@gmail.com>
2024-07-16 15:16:13 +0530

    user-profile and clerk added

---
+++ app/(profile)/layout.tsx
@@ -0,0 +1,11 @@
+export default function UserProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      {children}
+    </div>
+  );
+}
