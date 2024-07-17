7669d4c6a559e0ef61c32199b183e09babb3b606
Shashwat <shashwat12028@gmail.com>
2024-07-17 11:15:08 +0530

    Convex and clerk working functionality added for now

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      {children}
+    </div>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/recordings(.*)",
+  "/record(.*)",
+  "/action-items(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
