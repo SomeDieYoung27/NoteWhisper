4c19e5ba523c168281eae4be9ae795cfe8df6301
Shashwat <shashwat12028@gmail.com>
2024-07-26 19:35:02 +0530

    Action Item added

---
+++ app/(dashboard)/action-item/action-items-wrapper.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { type api } from "@/convex/_generated/api";
+import { EmptyState } from "@/components/skeltons";
+import {
+  type Preloaded,
+  usePaginatedQuery,
+  usePreloadedQuery,
+} from "convex/react";
+import NoteCard from "../recordings/[recordingId]/_components/note-card";
+
+export default function ActionItemsWrapper(props: {
+  preloadedActionItems: Preloaded<typeof api.notes.getActionItems>;
+}) {
+  const [search, setSearch] = useState("");
+  const userActionItems = usePreloadedQuery(props.preloadedActionItems);
+
+  if (userActionItems.length === 0) return <EmptyState />;
+
+  const actionItems = userActionItems.filter(
+    (e) =>
+      e.action.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+      e.title?.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+  );
+
+  return (
+    <div className="mx-auto max-w-xl space-y-4">
+      <h1 className="text-center text-2xl font-medium">Action Items</h1>
+      <h3 className="text-center text-gray-600 dark:text-gray-300">
+        {userActionItems?.length ?? 0} tasks
+      </h3>
+      <Input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search through action items..."
+      />
+      {actionItems.length === 0 ? (
+        <p className="text-center">No Result Found</p>
+      ) : (
+        actionItems.map((item) => <NoteCard key={item._id} {...item} />)
+      )}
+    </div>
+  );
+}
---
+++ app/(dashboard)/action-item/page.tsx
@@ -0,0 +1,14 @@
+import { getAuthToken } from "@/lib/serverUtils";
+import { preloadQuery } from "convex/nextjs";
+import { api } from "@/convex/_generated/api";
+import ActionItemsWrapper from "./action-items-wrapper";
+
+export default async function ActionItemsPage() {
+  const token = await getAuthToken();
+  const preloadedActionItems = await preloadQuery(
+    api.notes.getActionItems,
+    {},
+    { token }
+  );
+  return <ActionItemsWrapper preloadedActionItems={preloadedActionItems} />;
+}
